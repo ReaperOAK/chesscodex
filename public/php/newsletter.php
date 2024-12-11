9332a1b201d980ecb88b82a81452d1366784d6b0
ReaperOAK <oaak78692@gmail.com>
2024-12-11 23:17:11 +0530

    php fix?

--- public/php/newsletter.php
+++ public/php/newsletter.php
@@ -1,6 +1,9 @@
 <?php
 include 'config.php';
 
+// Set the Content-Type header to application/json
+header('Content-Type: application/json');
+
 // Get the email from the POST request
 $email = $_POST['email'] ?? '';
 
