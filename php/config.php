cb31e0383616e9561871fbca06c359b1722fdba3
ReaperOAK <136883103+ReaperOAK@users.noreply.github.com>
2024-12-11 18:02:19 +0000

    Build: (caa1848) newsletter fix?

--- php/config.php
+++ php/config.php
@@ -7,18 +7,8 @@ $pass = '1!jqkNyFs';
 // Create a new MySQLi connection
 $conn = new mysqli($host, $user, $pass, $db);
 
-// Check for connection errors
+// Check connection
 if ($conn->connect_error) {
-    error_log("Connection failed: " . $conn->connect_error);
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Set the charset to utf8mb4 for better security and compatibility
-if (!$conn->set_charset("utf8mb4")) {
-    error_log("Error loading character set utf8mb4: " . $conn->error);
-    die("Error loading character set utf8mb4: " . $conn->error);
-}
-
-// Set the session save path
-ini_set('session.save_path', '/opt/alt/php82/var/lib/php/session');
 ?>
