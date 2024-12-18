f77640e55083fbeff54be6f08bf0ff73df107e33
ReaperOAK <oaak78692@gmail.com>
2024-12-18 19:57:59 +0530

    navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           {/* Logo */}
-          <div className="flex items-center flex-shrink-0 mr-10">
+          <div className="flex items-center flex-shrink-0">
             <Link to="/" className="flex items-center text-2xl font-bold" aria-label="ChessCodex Home">
               <img src="/KCA_PNG.png" alt="ChessCodex Logo" className="h-10 w-auto mr-2" />
               ChessCodex
@@ -32,7 +32,7 @@ function Navbar() {
           </div>
 
           {/* Desktop Navigation */}
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-6">
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-4">
             {navLinks.map((link) => (
               <NavLink
                 key={link.to}
